c26647a1220b54c87e9a9517c796bd71af89f370
asif-imtiaz-udoy <asifimtiaz240@gmail.com>
2022-06-30 21:32:23 +0600

    register and define middleware

--- routes/web.php
+++ routes/web.php
@@ -16,33 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('first');
 
-Route::get('/product/{id?}', function ($id = 'all') {
-    $data = [
-        1 => 'Apple',
-        2 => 'Orange',
-        3 => 'Banana'
-    ];
-
-    if ($id == 'all') {
-        return $data;
-    } else {
-        return $data[$id] ?? "The fruit is not in the basket";
-    }
-});
-
-Route::name('admin.')->prefix('admin')->group(function() {
-    Route::get('/group-test', function(){
-        return 'Hello admin';
-    })->name('first');
-    Route::get('/group-test1', function(){
-        return 'Hello admin 1';
-    })->name('second');
-});
-
-Route::get('/group-test', function(){
-    return 'Hello User';
-})->name('first');
-
-Route::view('/first', 'first');
+Route::get('/noaccess', function() {
+    return 'You do not have the access in this application';
+})->name('noaccess');
