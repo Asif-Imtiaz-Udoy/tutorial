0b0589723333a838d39b848b4630146094ea5cb8
asif-imtiaz-udoy <asifimtiaz240@gmail.com>
2022-07-01 01:58:18 +0600

    middleware types

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->middleware('first');
+})->middleware('group');
+
 
 Route::get('/noaccess', function() {
     return 'You do not have the access in this application';
